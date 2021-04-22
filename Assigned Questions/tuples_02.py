f39511076c66b5f3f0b4d3ef8b1a10a762c88731
Vinuthna <gvvlvinuthna2000@gmail.com>
2021-04-22 22:20:51 +0530

    Solved Tuple questions.

--- Assigned Questions/tuples_02.py
+++ Assigned Questions/tuples_02.py
@@ -12,3 +12,16 @@ for i in list_:
     dict_[i[0]] = i[1]
 print("After conversion: ")
 print(dict_)
+
+# Sample Input
+
+# Enter no. of tuples: 2
+# Enter tuple: A 1
+# Enter tuple: B 2
+
+# Output
+
+# Before conversion:
+# [('A', '1'), ('B', '2')]
+# After conversion:
+# {'A': '1', 'B': '2'}
