5e406d05f098b2f6706fd5cd4943379b96446853
Vinuthna <gvvlvinuthna2000@gmail.com>
2021-04-24 14:30:10 +0530

    Added Modifying_string.py

---
+++ Modifying_string.py
@@ -0,0 +1,20 @@
+# To change a character of a string at a given position.
+
+def modify_string(s, pos, ch):
+    list_ = list(s)
+    list_[pos] = ch
+    return "".join(list_)
+
+if __name__ == "__main__":
+    s = input("Enter the string: ")
+    pos = int(input("Enter position: "))
+    ch = input("Enter the character: ")
+    print(modify_string(s, pos, ch))
+
+
+# Sample Input
+# Enter the string: modify
+# Enter position: 2
+# Enter the character: f
+# Output
+# mofify
