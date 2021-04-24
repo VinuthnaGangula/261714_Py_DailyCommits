82b1d2283eb7dd2c41e9ae97f010e5922ae2ed56
Vinuthna <gvvlvinuthna2000@gmail.com>
2021-04-24 23:42:14 +0530

    Added formatting.py

---
+++ formatting.py
@@ -0,0 +1,16 @@
+# print 1 to n numbers in different number format using string formatting.
+
+def print_formatted(n):
+    w = len("{0:b}".format(n))
+    for i in range(1, n + 1):
+        print(f"{i: {w}d} {i: {w}o} {i: {w}X} {i: {w}b}")
+
+if __name__ == "__main__":
+    number = int(input("Enter the number: "))
+    print_formatted(number)
+
+# Sample Input
+# Enter the number: 2
+# Output
+# 1  1  1  1
+#  2  2  2  10
